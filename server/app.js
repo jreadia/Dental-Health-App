06cce0bbcee89ab63de4c69293ea91d198b28404
James Russel E. Adia <adiajames272@gmail.com>
2026-06-09 23:54:17 +0800

    Update app.js

--- server/app.js
+++ server/app.js
@@ -25,6 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(cookieParser());
+app.set('trust proxy', 1); // Trust the first proxy (Render)
 app.use(globalLimiter);
 
 // Routes
